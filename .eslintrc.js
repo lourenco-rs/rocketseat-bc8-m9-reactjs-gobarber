edb422fcb115f9790ca1079a582305be099d7f3f
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-03 10:32:37 -0300

    Utilizando Root Import

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,5 +39,12 @@ module.exports = {
     ],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn'
+  },
+  settings: {
+    "import/resolver": {
+      "babel-plugin-root-import": {
+        rootPathSuffix: "src"
+      }
+    }
   }
 };
