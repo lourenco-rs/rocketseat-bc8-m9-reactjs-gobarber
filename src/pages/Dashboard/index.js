89071973b6a42480620838f393fc1f7561343211
Rodrigo Lourenço <lourenco.rs@gmail.com>
2019-10-03 23:26:13 -0300

    Requisições autenticadas

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import api from '~/services/api';
 
 // import { Container } from './styles';
 
 export default function Dashboard() {
+  // para testar o envio do bearer token na requisição
+  api.get('appointments');
+
   return <h1>Dashboard</h1>;
 }
